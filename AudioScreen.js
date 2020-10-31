00bd9edd722ad37f82268cab2642093df18c1ab3
Juliano Júnio de Freitasm <juliano.junio.freitas@gmail.com>
2020-10-31 00:08:44 -0300

    Implementação do React Native Track Player para a tela de Audio

--- AudioScreen.js
+++ AudioScreen.js
@@ -1,22 +1,162 @@
-import React from "react";
-import Video from 'react-native-video';
-import { View } from "react-native";
+import React, { useEffect, useState } from "react";
+import TrackPlayer from 'react-native-track-player';
+import { Button, View, Text, StyleSheet, Image } from "react-native";
+import { useTrackPlayerProgress } from 'react-native-track-player/lib/hooks';
+import { useTrackPlayerEvents } from 'react-native-track-player/lib/hooks';
+import { TrackPlayerEvents, STATE_PLAYING } from 'react-native-track-player';
 
-const AudioScreen = () => (
-  <View style={{ backgroundColor: '#312e38', flex: 1 }}>
-    <Video 
-      source={{
-        uri: 'http://streaming05.zas.media:8053/live.m3u',
-        type: 'm3u8' 
-      }}
-      controls
-      resizeMode="contain"
-      style={{
-        flex: 1,
-      }}
-      progressUpdateInterval={5000}
-      />
-  </View>
+import Slider from '@react-native-community/slider';
+
+const trackPlayerInit = async () => {
+ await TrackPlayer.setupPlayer();
+ await TrackPlayer.add({
+   id: '1',
+   url: 'http://streaming05.zas.media:8053/live',
+   type: 'default',
+   title: 'My Title',
+   album: 'My Album',
+   artist: 'Rohan Bhatia',
+   artwork: 'https://picsum.photos/100',
+ });
+ return true;
+};
+
+TrackPlayer.updateOptions({
+  stopWithApp: true,
+  capabilities: [
+    TrackPlayer.CAPABILITY_PLAY,
+    TrackPlayer.CAPABILITY_PAUSE,
+    TrackPlayer.CAPABILITY_JUMP_FORWARD,
+    TrackPlayer.CAPABILITY_JUMP_BACKWARD,
+  ],
+});
+
+
+const AudioScreen = () => {
+  const [isTrackPlayerInit, setIsTrackPlayerInit] = useState(false);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [sliderValue, setSliderValue] = useState(0);
+  const [isSeeking, setIsSeeking] = useState(false);
+  const {position, duration} = useTrackPlayerProgress(250);
+
+  useTrackPlayerEvents([TrackPlayerEvents.PLAYBACK_STATE], event => {
+    if (event.state === STATE_PLAYING) {
+      setIsPlaying(true);
+    } else {
+      setIsPlaying(false);
+    }
+  });
+
+  useEffect(() => {
+    const startPlayer = async () => {
+        let isInit =  await trackPlayerInit();
+        setIsTrackPlayerInit(isInit);
+    }
+
+    startPlayer();
+  }, []);
+
+  useEffect(() => {
+    if (!isSeeking && position && duration) {
+      setSliderValue(position / duration);
+    }
+  }, [position, duration]);
+
+  const onButtonPressed = () => {
+    if (!isPlaying) {
+      TrackPlayer.play();
+      setIsPlaying(true);
+    } else {
+      TrackPlayer.pause();
+      setIsPlaying(false);
+    }
+  };
+
+  const slidingStarted = () => {
+    setIsSeeking(true);
+  };
+
+  const slidingCompleted = async value => {
+    await TrackPlayer.seekTo(value * duration);
+    setSliderValue(value);
+    setIsSeeking(false);
+  };
+
+  return (
+    <View style={styles.mainContainer}>
+      <View style={styles.imageContainer}>
+        <Image
+          source={{
+            uri: 'http://ieadi.com.br/images/logos/logo-radio-vale.png',
+          }}
+          resizeMode="contain"
+          style={styles.albumImage}
+        />
+      </View>
+      <View style={styles.detailsContainer}>
+        <Text style={styles.songTitle}>Radio Vale</Text>
+        <Text style={styles.artist}>Aleluia Music</Text>
+      </View>
+      <View style={styles.controlsContainer}>
+        <Slider
+          style={styles.progressBar}
+          minimumValue={0}
+          maximumValue={1}
+          value={sliderValue}
+          minimumTrackTintColor="#111000"
+          maximumTrackTintColor="#000000"
+          onSlidingStart={slidingStarted}
+          onSlidingComplete={slidingCompleted}
+          thumbTintColor="#000"
+        />
+        <Button
+          title={isPlaying ? 'Pause' : 'Play'}
+          onPress={onButtonPressed}
+          style={styles.playButton}
+          disabled={!isTrackPlayerInit}
+          color="#000000"
+        />
+      </View>
+    </View>
   );
+
+}
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    backgroundColor: '#EDEDED',
+  },
+  imageContainer: {
+    flex: 0.5,
+    justifyContent: 'center',
+  },
+  detailsContainer: {
+    flex: 0.05,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  controlsContainer: {
+    flex: 0.45,
+    justifyContent: 'flex-start',
+  },
+  albumImage: {
+    width: 250,
+    height: 250,
+    alignSelf: 'center',
+    borderRadius: 40,
+  },
+  progressBar: {
+    height: 20,
+    paddingBottom: 90,
+  },
+  songTitle: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  artist: {
+    fontSize: 14,
+  },
+});
 
 export default AudioScreen;
